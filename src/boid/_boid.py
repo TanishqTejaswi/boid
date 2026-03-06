cf87c716de7afcd3cfadf67724f5bd310e76d84a
Tanishq <tanishq.tejaswi@outlook.com>
2026-03-06 15:48:29 +0100

    Added code for "Boid" class

--- src/boid/_boid.py
+++ src/boid/_boid.py
@@ -1,5 +1,54 @@
-class Boid:
-    def __init__(self, *args):
-        print("Hello from Boid")
-        self.arg = args[0]
+import numpy as np
 
+class Boid:
+
+    @property
+    def acceleration(self):
+        alpha = np.pi/30
+        a = np.array([0, 0], dtype=float)
+        a[0] = self.velocity[0]*(np.cos(alpha)-1) - self.velocity[1]*np.sin(alpha)
+        a[1] = self.velocity[0]*np.sin(alpha) + self.velocity[1]*(np.cos(alpha)-1)
+        return a
+
+    def update_velocity(self, *, dt: float) -> None:
+        """
+        Update the velocity of the boid based on its acceleration
+
+        Args:
+            dt: The time step for the simulation
+        """
+        # Copy the function from the previous cell
+        self.velocity = self.velocity + dt*self.acceleration
+        
+    def move_boid(self, *, dt: float) -> None:
+        """
+        Move the boid based on its velocity
+
+        Args:
+            dt: The time step for the simulation
+        """
+        # Write a function that moves the boid based on its velocity
+        self.position = self.position + dt*self.velocity
+        self.update_velocity(dt=dt)
+
+    def plot_trajectory(self, *, dt: float, num_steps: int) -> None:
+        """
+        Plot the trajectory of the boid
+
+        Args:
+            dt: The time step for the simulation
+            num_steps: The number of steps to simulate
+        """
+        # Write the code to plot the trajectory of the boid
+        traj = []
+        for _ in range(num_steps):
+            self.move_boid(dt=dt)
+            traj.append(self.position)
+        traj = np.array(traj)
+        _, ax = plt.subplots()
+        ax.plot(*traj.T, "o-")
+        ax.set_aspect("equal")
+
+    def __init__(self, position, velocity):
+        self.position = np.array(position, dtype=float)
+        self.velocity = np.array(velocity, dtype=float)
